c8d8f3545384d17f60bf6f366d761c947b477106
Chrisson <77775011+Chrisson543@users.noreply.github.com>
2024-01-07 02:56:00 +0100

    Update firebase-config.jsx

--- src/backend/firebase-config.jsx
+++ src/backend/firebase-config.jsx
@@ -4,12 +4,12 @@ import { collection, getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD56KNI-3ZV4LSneUQuEBJL5TITeFRG8Ck",
-  authDomain: "chat-app-4e835.firebaseapp.com",
-  projectId: "chat-app-4e835",
-  storageBucket: "chat-app-4e835.appspot.com",
-  messagingSenderId: "962763349503",
-  appId: "1:962763349503:web:151e5ed7aa59d38a9e1bca"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
